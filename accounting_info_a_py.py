2d2e63ba650d3f292018c84f9aa13471a1d74505
vamsi-4-3-4 <94716145+vamsi-4-3-4@users.noreply.github.com>
2022-08-06 18:27:05 +0530

    Create accounting_info_a_py.py

---
+++ accounting_info_a_py.py
@@ -0,0 +1,20 @@
+assets=["cash","short_term_investments","accounts_receivable","notes_receivable","inventory_to_be_sold","supplies","prepaid_expenses","long_term_investments","equipment","buildings","leased_assets","land","intangibles"]
+liabilities=["accounts_payable","accured_expenses_payable","notes_payable","taxes_payable","lease_liabilities","unearned_revenue","bonds_payable"]
+stockholdersequty=["common_stock","additional_paid_in_capital","retained_earnings"]
+revenues=["sales_revenue","free_revenue","interest_revenue","rent_revenue","service_revenue"]
+expenses=["cost_of_goods_sold","wages_expense","rent_expense","interest_expense","depreciation_expense","advertising_expense","insurance_expense","repairs_expense","income_tax_expense"]
+print("ASSETS ARE\n")
+for item in assets:
+    print(item)
+print("\nLIABILITIES ARE\n")
+for item in liabilities:
+    print(item)
+print("\nSHARE HOLDERS EQUITIES ARE\n")
+for item in stockholdersequty:
+    print(item)
+print("\nREVENUES ARE\n")
+for item in revenues:
+    print(item)
+print("\nEXPENSES ARE\n")
+for item in expenses:
+    print(item)
