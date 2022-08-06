3598579eb7b26dd147657c78221036ba488cf14b
vamsi-4-3-4 <94716145+vamsi-4-3-4@users.noreply.github.com>
2022-08-06 18:27:50 +0530

    Create accounting_info_a_js.js

---
+++ accounting_info_a_js.js
@@ -0,0 +1,52 @@
+const assets=[
+    "cash",
+    "short_term_investments",
+    "accounts_receivables",
+    "notes_receivable",
+    "inventory_to_be_sold",
+    "supplies",
+    "prepaid_expenses",
+    "long_term_investments",
+    "equipment",
+    "buildings",
+    "leased_assets",
+    "land",
+    "intangibles"
+]
+liabilities=[
+    "accounts_payable",
+    "accured_expenses_payable",
+    "notes_payable",
+    "taxes_payable",
+    "lease_liabilities",
+    "unearned_revenue",
+    "bonds_payable"
+]
+shareholdersequties=[
+    "common_stock",
+    "additional_paid_in_capital",
+    "retained_earnings"
+]
+revenues=[
+    "sales_revenue",
+    "free_revenue",
+    "interest_revenue",
+    "rent_revenue",
+    "service_revenue"
+]
+expenses=[
+    "cost_of_goods_sold",
+    "wages_expense",
+    "rent_expense",
+    "interest_expense",
+    "depreciation_expense",
+    "advertising_expense",
+    "insurance_expense",
+    "repairs_expense",
+    "income_tax_expense"
+]
+console.log("ÄSSETS ARE\n",assets)
+console.log("LIABILITIES ARE\n",liabilities)
+console.log("SHARE HOLDERS EQUITIES ARE\n",shareholdersequties)
+console.log("REVENUES ARE\n",revenues)
+console.log("EXPENSES ARE\n",expenses)
